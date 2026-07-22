f94761562f39a67a3f836b151057f1afa4fbe59b
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-07-23 02:02:31 +0600

    fixt

--- backend/controllers/todoControllers.js
+++ backend/controllers/todoControllers.js
@@ -1,58 +1,122 @@
-const Todo = require('../models/todoModel')
+const Todo = require('../models/todoModel');
 
+// Create Todo
+const createTodo = async (req, res) => {
+  try {
+    const { task, priority } = req.body;
 
-const createTodo = (req,res)=>{
-    const {task,status,priority} = req.body  
-
-
-    if(!task || !priority){
-        return res.json({
-            success: false,
-
-            massage: 'plase fill the all fields'
-        })
+    if (!task || !priority) {
+      return res.status(400).json({
+        success: false,
+        message: 'Please fill all the fields',
+      });
     }
 
-    const todo = new Todo ({
-        task: task,
-        priority: priority,
-        path:req.file.path
-    })
+    const todo = new Todo({
+      task,
+      priority,
+    });
 
-    todo.save()
+    await todo.save();
 
     res.status(201).json({
-        success:true,
-        massage: 'Todo Created'
-
-    })
-
-}
-
-const allTodos = async (req,res)=>{
-    let data = await Todo.find({})
-    res.json({
-        success:true,
-        message: "Todo Collected",
-        data : data
-    })
-}
-
-const deletUser = async (req,res)=>{
-    const {id} = req.params
-    await Todo.findByIdAndDelete(id)
-    res.json('User Deleted')
-}
-
-
-
-const updateData = async (req, res) => {
-    const { id } = req.params;
-    const updateTask = await Todo.findByIdAndUpdate({_id:id},req.body)
-    res.json({
-        success: true,
-        message: "Todo updated successfully"
+      success: true,
+      message: 'Todo Created Successfully',
+      data: todo,
     });
-}
+  } catch (error) {
+    console.log(error);
 
-module.exports = {createTodo,allTodos,deletUser,updateData}
+    res.status(500).json({
+      success: false,
+      message: 'Internal Server Error',
+    });
+  }
+};
+
+// Get All Todos
+const allTodos = async (req, res) => {
+  try {
+    const data = await Todo.find({});
+
+    res.status(200).json({
+      success: true,
+      message: 'Todos Collected Successfully',
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      success: false,
+      message: 'Internal Server Error',
+    });
+  }
+};
+
+// Delete Todo
+const deleteTodo = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const todo = await Todo.findByIdAndDelete(id);
+
+    if (!todo) {
+      return res.status(404).json({
+        success: false,
+        message: 'Todo Not Found',
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: 'Todo Deleted Successfully',
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      success: false,
+      message: 'Internal Server Error',
+    });
+  }
+};
+
+// Update Todo
+const updateData = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatePayload = { ...req.body };
+
+    if (req.file) {
+      updatePayload.image = req.file.path;
+    }
+
+    const updatedTodo = await Todo.findByIdAndUpdate(id, updatePayload, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updatedTodo) {
+      return res.status(404).json({
+        success: false,
+        message: 'Todo Not Found',
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: 'Todo Updated Successfully',
+      data: updatedTodo,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      success: false,
+      message: 'Internal Server Error',
+    });
+  }
+};
+
+module.exports = { createTodo, allTodos, deleteTodo, updateData };
